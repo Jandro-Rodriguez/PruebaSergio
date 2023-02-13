203df0d41d6e94bf9877aadaac4d73de3791ab1b
Jandro-Rodriguez <jandro_r10@hotmail.com>
2023-02-13 20:14:47 +0100

    alejandro cambio colores
    ahí estan mis huevos pelaos!!!

--- ProyectoPrueba/resources/views/main.blade.php
+++ ProyectoPrueba/resources/views/main.blade.php
@@ -1,40 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
     <title>Document</title>
 </head>
+
 <body>
-    <header>
-        <h1>Recuerda tu graduación</h1>
-    </header>
+    <div class="parent">
 
-    <div class="menu">
-        <nav>
-            <ul>
-              <li><a href="#">Inicio</a></li>
-              <li><a href="#">Nuestro equipo</a></li>
-              <li><a href="#">Proyectos</a></li>
-              <li><a href="#">Contacto</a></li>
-            </ul>
-        </nav>
+        <header>
+            <h1>Recuerda tu graduación</h1>
+        </header>
+
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Nuestro equipo</a></li>
+                    <li><a href="#">Proyectos</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="content">
+            <p>
+                Lorem ipsum, dolor sit amet consecteturtenetur
+                asperiores reiciendis iustom dolores
+                recusandae sit sequi? Consectetur!
+            </p>
+
+            <button type="button" class="btn btn-outline-warning">Warning</button>
+
+        </div>
+        <footer>Somos el equipo de folletis</footer>
+
+
     </div>
-
-    <div class="content">
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam distinctio possimus velit! Nemo tenetur asperiores reiciendis iusto, numquam nam delectus itaque perspiciatis eveniet in quibusdam dolores recusandae sit sequi? Consectetur!
-        </p>
-    </div>
-
-    <footer>Somos el equipo de folletis</footer>
-    
-
-    
-
-    
 
 
 </body>
+
 </html>
